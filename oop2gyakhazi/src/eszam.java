9e381e9dd8e2c17851b1842bbb138cb8f487b1e3
torpe <torpe@LAPTOP-9G0TQ90A>
2018-02-23 21:55:00 +0100

    Javítás default package-re

---
+++ oop2gyakhazi/src/eszam.java
@@ -0,0 +1,22 @@
+public class eszam {
+
+	public static void main(String[] args) {
+		int darab = 1000;
+		double e = 0;
+		
+		for (int i = 0; i <= darab; i++) {
+			double f = Fakt(i);
+			e += 1.0/f;
+		}
+		System.out.println("Az Euler féle e szám: "+e);
+
+	}
+	public static double Fakt(int a) {
+		double f = 1;
+		for (int i = 1; i <= a; i++) {
+			f *= i;
+		}
+		return f;
+	}
+
+}
